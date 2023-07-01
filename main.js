9f68d63ca4a4f6f4102a8c719ee42beb73fabf4f
taylorlottdixon <136010920+taylorlottdixon@users.noreply.github.com>
2023-06-30 22:12:50 -0500

    UI updates

---
+++ main.js
@@ -0,0 +1,23 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+let board
+let secretCode
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+function initLanding() {
+    createLandingPage()
+}
+
+function createLandingPage() {
+    const landingCupcakes = document.createElement("div")
+    
+}
